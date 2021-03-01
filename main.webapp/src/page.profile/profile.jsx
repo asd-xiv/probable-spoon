b50de21f03652efa1f08e869a72fad83641a10bc
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2021-03-01 20:49:53 +0100

    chore: Cleanup before submodule link

--- main.webapp/src/page.profile/profile.jsx
+++
@@ -1,9 +0,0 @@
-const debug = require("debug")("probable-spoon:ProfilePage")
-
-import React from "react"
-
-const ProfilePage = ({}) => {
-  return <div>Profile Page</div>
-}
-
-export { ProfilePage }
